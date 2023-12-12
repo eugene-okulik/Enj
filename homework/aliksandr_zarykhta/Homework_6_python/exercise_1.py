b7f1a80ff25f1eaf7a355d8dbfc3406f8c3b3f71
Aliaksandr Zarykhta <zaraza48a@gmail.com>
2023-12-12 23:56:26 +0300

    make homework 6 conditions & loops in python

--- homework/aliksandr_zarykhta/Homework_6_python/exercise_1.py
+++ homework/aliksandr_zarykhta/Homework_6_python/exercise_1.py
@@ -1,9 +1,9 @@
-text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. \
+text_task = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. \
     Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
 
-text = text.replace(',', 'ing,')
-text = text.replace('.', 'ing.')
-modified_text = text.split()
+text_task = text_task.replace(',', 'ing,')
+text_task = text_task.replace('.', 'ing.')
+modified_text = text_task.split()
 for word in modified_text:
     if 'ing' in word:
         print(word, end=' ')
